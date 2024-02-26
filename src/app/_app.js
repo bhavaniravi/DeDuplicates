9ec86f58fcda666c4fc51efa5e68d2a95c5a00c3
Bhavani Ravi <bhava0895@gmail.com>
2024-02-26 19:12:04 +0530

    chore: add google ads

---
+++ src/app/_app.js
@@ -0,0 +1,7 @@
+import { Script } from "next";
+
+<Script
+  async
+  src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5847967413895882"
+  crossorigin="anonymous"
+></Script>;
