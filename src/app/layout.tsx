d928dd8c79483aa2351d723184c704cb54331fe0
Bhavani Ravi <bhava0895@gmail.com>
2024-02-27 06:45:41 +0530

    chore: add google adsense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,17 +16,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5847967413895882"
-          crossOrigin="anonymous"
-        ></script>
-        <meta
-          name="google-adsense-account"
-          content="ca-pub-5847967413895882"
-        ></meta>
-      </head>
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5847967413895882"
+        crossOrigin="anonymous"
+      ></script>
+      <meta
+        name="google-adsense-account"
+        content="ca-pub-5847967413895882"
+      ></meta>
       <body className={inter.className}>{children}</body>
     </html>
   );
