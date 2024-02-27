13a59aa2ff06bed58ef37251761caf2c84356071
Bhavani Ravi <bhava0895@gmail.com>
2024-02-27 06:33:54 +0530

    chore: add adsense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5847967413895882"
+          crossOrigin="anonymous"
+        ></script>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
