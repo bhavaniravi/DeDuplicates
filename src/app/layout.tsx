6ca84a22ee50711e6f2322c1f871b2984fbfdbbc
Bhavani Ravi <bhava0895@gmail.com>
2024-02-27 06:37:42 +0530

    chore: add metatag:

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,10 @@ export default function RootLayout({
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5847967413895882"
           crossOrigin="anonymous"
         ></script>
+        <meta
+          name="google-adsense-account"
+          content="ca-pub-5847967413895882"
+        ></meta>
       </head>
       <body className={inter.className}>{children}</body>
     </html>
